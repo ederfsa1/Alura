afaf7c4c3ac5a3be0a31f6404f686f042eaccfa2
Ederson Silva <esilva13@unifesp.br>
2022-08-03 17:07:54 -0300

    aula de operadores e funções

---
+++ aula2.js
@@ -0,0 +1,11 @@
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+// Vamos calcular a área de um retângulo
+let largura = "10";
+let altura = "5";
+console.log(Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da da multiplicação
+
+let meuNome = "leonardo";
+console.log(Number(meuNome)); // como a variável meuNome não contém apenas números ele retorna o erro NaN (Not a Number, não é número);
+console.log( + meuNome); // a conversão também retornará NaN
---
+++ programa.js
@@ -0,0 +1,32 @@
+function soma (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(soma(8,8)) 
+
+function cumprimentar(){
+    console.log('oi gente!')
+   }    
+   cumprimentar() 
+
+   function cumprimentaPessoa(pessoa){
+    console.log(`oi, ${pessoa}!`)
+   }
+   
+   cumprimentaPessoa('Helena')
+
+   function cumprimentar(){
+    return 'Oi gente!'
+   }
+   
+   function cumprimentaPessoa(nomePessoa) {
+    console.log(`${cumprimentar()} Meu nome é ${nomePessoa}`)
+   }
+   
+   cumprimentaPessoa('Paula') // “Oi gente! Meu nome é Paula”
+
+   function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   operacaoMatematica(15, 30, 45) // 90
